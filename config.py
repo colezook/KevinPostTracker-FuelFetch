323652d89101ac9d06ebeaa73b8f3ba6120ca874
CZ <colemzuckowsky@gmail.com>
2024-10-16 22:07:44 +0000

    DB table 'posts' column reordering

--- config.py
+++ config.py
@@ -4,23 +4,11 @@ import os
 USER_IDS = [
     "56413349678",
     "1671387607",
-    "11706414183",
-    "60460786182",
-    "2940087474",
-    "50062729418",
-    "27451965377",
-    "45778329299",
-    "64398516412",
-    "45098556",
-    "67467810580",
-    "63504017887",
-    "66846646498",
-    "55349692730",
 ]
 
 
 # Number of days to look back for old posts
-DAYS_TO_LOOK_BACK = 100
+DAYS_TO_LOOK_BACK = 15
 
 # You can add other configuration variables here if needed
 API_BASE_URL = "https://api.hikerapi.com/v2/user/clips"
